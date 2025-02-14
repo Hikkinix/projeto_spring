fa05c7f2d3c15c1da731c2745493d8ce0c4bc33d
Olimpio Neto <olimpio.neto@edu.unipar.br>
2025-02-14 10:09:27 -0300

    Adicionar Aspecto para pratica

---
+++ gameslist/src/main/java/com/praticas/gameslist/aspects/GameAspect.java
@@ -0,0 +1,16 @@
+package com.praticas.gameslist.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class GameAspect {
+
+    @Before("execution(* com.praticas.gameslist.services.*.findAll*(..))")
+    public void registrarLog() {
+        System.out.println("Consulta Reaelizada com sucesso");
+    }
+
+}
